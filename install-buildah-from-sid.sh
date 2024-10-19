5f463da92f050247aee50f81730dab33cc985a3d
Robert Detjens <github@detjens.dev>
2024-10-19 16:42:02 -0700

    Install newer Buildah from Debian Unstable
    Debian and Ubuntu builds of Buildah patch out heredoc support because of a old
    Docker / Buildkit frontend parser version. In some future Ubuntu release this
    will be fixed as a newer Docker has landed in upstream Debian Sid/Unstable.
    (see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1072578)
    
    Until then, install buildah from Sid to get heredoc support.
    
    Signed-off-by: Robert Detjens <github@detjens.dev>

---
+++ install-buildah-from-sid.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Debian and Ubuntu builds of Buildah patch out heredoc support because of a old
+# Docker / Buildkit frontend parser version. In some future Ubuntu release this
+# will be fixed as a newer Docker has landed in upstream Debian Sid/Unstable.
+# (see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1072578)
+#
+# Until then, install buildah from Sid to get heredoc support.
+
+sudo apt update
+sudo apt install debian-keyring debian-archive-keyring
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/debian-sid.sources
+Types: deb
+# http://snapshot.debian.org/archive/debian/20241016T000000Z
+URIs: http://deb.debian.org/debian
+Suites: sid
+Components: main
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
+cat <<EOF | sudo tee /etc/apt/preferences.d/debian-sid.pref
+Package: *
+Pin: release o=Debian,a=unstable,n=sid
+Pin-Priority: 100
+EOF
+
+sudo apt update
+sudo apt install -t sid buildah
